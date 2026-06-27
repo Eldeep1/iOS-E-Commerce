6e7284eac724665172bab245cbdddf2c508027ac
depo <alieldeep111@gmail.com>
2026-06-27 17:42:42 +0300

    creating the use cases

---
+++ Buyza App/Authentication/Domain/Usecase/Auth/RegisterUseCase.swift
@@ -0,0 +1,34 @@
+//
+//  RegisterUseCase.swift
+//  Buyza App
+//
+//  Created by depo on 27/06/2026.
+//
+
+import Foundation
+
+
+protocol RegisterUseCaseProtocol {
+    func execute(email: String, password: String, name:String) async throws -> UserModel
+}
+
+struct RegisterUseCase: RegisterUseCaseProtocol {
+    
+    private let authRepository: AuthRepoProtocol
+    
+    init(authRepository: AuthRepoProtocol) {
+        self.authRepository = authRepository
+    }
+    
+    func execute(email: String, password: String, name: String) async throws -> UserModel {
+        guard email.contains("@") && email.count > 5 else {
+            throw AuthError.invalidEmail
+        }
+        
+        do {
+            return try await authRepository.createUser(email: email, password: password, name: name)
+        } catch {
+            throw AuthError.map(error)
+        }
+    }
+}
