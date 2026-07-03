48c823edc54673e00643c0c60013583fbfc74ab2
AhmedTarek215 <at037940@gmail.com>
2026-07-03 17:01:52 +0300

    Implement ProductEntity inthe database

---
+++ Buyza App/Buyza App/Domain/Models/ProductEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ProductEntity+CoreDataClass.swift
+//  Buyza App
+//
+//  Created by Ahmed Tarek on 03/07/2026.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ProductEntity)
+public class ProductEntity: NSManagedObject {
+
+}
